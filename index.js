ef2f3e8f2c799ca3c2ab82b231bf0b49bd047ae4
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2025-02-23 16:07:13 +0530

    home page using server side rendering

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = 8001;
 const connectMongoDB = require("./connect");
 const urlRoute = require("./routes/url");
 const path = require("path");
-const URL = require("./models/url");
 
 const userRoute = require("./routes/user");
 const staticRoute = require("./routes/staticRouter");
@@ -20,7 +19,9 @@ connectMongoDB("mongodb://127.0.0.1:27017/url-shortener")
 app.use(express.json());
 
 app.set("view engine", "ejs");
+
 app.set("views", path.resolve("./views"));
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
