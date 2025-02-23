6262e32b57498dce14ab3a172a9d7c847ce5a0b9
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2025-02-24 01:07:00 +0530

    statefull authentication

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const port = 8001;
 const connectMongoDB = require("./connect");
 const urlRoute = require("./routes/url");
 const path = require("path");
-
+const cookieParser = require('cookie-parser')
 const userRoute = require("./routes/user");
 const staticRoute = require("./routes/staticRouter");
-
+const {restrictToLoggedinUserOnly} = require('./middleware/auth')
 connectMongoDB("mongodb://127.0.0.1:27017/url-shortener")
   .then(() => {
     console.log("Connected to MongoDB");
@@ -23,8 +23,9 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 
-app.use("/url", urlRoute);
+app.use("/url",  restrictToLoggedinUserOnly , urlRoute);
 app.use("/user", userRoute);
 app.use("/", staticRoute);
 app.use("/analytics/:shortId" , urlRoute)
