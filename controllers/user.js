df49535c3bb1219ca6bb3419de50261815950997
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2025-02-24 13:31:27 +0530

    auth service

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const User = require("../models/user");
 const { v4: uuidv4 } = require("uuid");
-const {setUser , getUser} = require('../service/auth')
+const { setUser, getUser } = require("../service/auth");
 async function handleUserSignUp(req, res) {
   const { name, email, password } = req.body;
   await User.create({
@@ -17,9 +17,8 @@ async function handleUserLogin(req, res) {
     return res.render("login", {
       error: "Invalid UserName or password",
     });
-  const sessionId = uuidv4();
-  setUser(sessionId , user)
-  res.cookie("uuid",sessionId)
+  const token = setUser(user);
+  res.cookie("uuid", token);
   return res.redirect("/");
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,24 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken')
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+const secret = "AkashSingh"
+
+function setUser( user) {
+
+  return jwt.sign({
+    _id : user._id,
+    email:user.email,
+
+  }, secret)
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if(!token) return null
+  try {
+    return jwt.verify(token , secret)
+  } catch (error) {
+    return null
+  }
+
 }
 
 module.exports = {
